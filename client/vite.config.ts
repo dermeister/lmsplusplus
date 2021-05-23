6e50e2c8fa3d36df44028d1090c17309fc839691
Denis Duzh <duzh.denis@gmail.com>
2021-05-23 11:26:23 +0300

    feat: add reactronic

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,6 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
   build: { target: "es2015" },
+  esbuild: { target: "es2015" },
   plugins: [reactRefresh()],
 });
