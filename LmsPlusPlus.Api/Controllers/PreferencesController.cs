4877972482bff75689a276d1c9c71b1b2d296bfc
Denis Duzh <duzh.denis@gmail.com>
2022-05-15 21:26:41 +0300

    refactor: PreferencesController

--- LmsPlusPlus.Api/Controllers/PreferencesController.cs
+++ LmsPlusPlus.Api/Controllers/PreferencesController.cs
@@ -4,26 +4,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LmsPlusPlus.Api;
 
-[ApiController, Route("preferences"), Authorize(Roles = "Author, Solver, Admin")]
+[ApiController, Route("preferences")]
 public class PreferencesController : ControllerBase
 {
     readonly Infrastructure.ApplicationContext _context;
 
     public PreferencesController(Infrastructure.ApplicationContext context) => _context = context;
 
-    [HttpGet]
+    [HttpGet, Authorize(Roles = "Author, Solver, Admin")]
     public async Task<Response.Preferences> Get()
     {
-        long id = Utils.GetUserIdFromClaims(User);
-        Infrastructure.Preferences preferences = await _context.Preferences.SingleAsync(p => p.UserId == id);
+        AuthorizationCredentials credentials = new(User);
+        Infrastructure.Preferences preferences = await _context.Preferences.SingleAsync(p => p.UserId == credentials.UserId);
         return (Response.Preferences)preferences;
     }
 
-    [HttpPut]
+    [HttpPut, Authorize(Roles = "Author, Solver, Admin")]
     public async Task<Response.Preferences> Update(Request.Preferences requestPreferences)
     {
-        long id = Utils.GetUserIdFromClaims(User);
-        Infrastructure.Preferences databasePreferences = await _context.Preferences.SingleAsync(p => p.UserId == id);
+        AuthorizationCredentials credentials = new(User);
+        Infrastructure.Preferences databasePreferences = await _context.Preferences.SingleAsync(p => p.UserId == credentials.UserId);
         databasePreferences.Theme = requestPreferences.Theme;
         await _context.SaveChangesAsync();
         return (Response.Preferences)databasePreferences;
