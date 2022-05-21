b7d375a2692af003980a95636caf7b3ae70bb6dd
Denis Duzh <duzh.denis@gmail.com>
2022-05-21 13:23:55 +0300

    feat: add endpoint for setting active account

--- LmsPlusPlus.Api/Controllers/VcsAccountsController.cs
+++ LmsPlusPlus.Api/Controllers/VcsAccountsController.cs
@@ -20,6 +20,27 @@ public class VcsAccountsController : ControllerBase
                       select (Response.VcsAccount)a).ToArrayAsync();
     }
 
+    [HttpPut("{accountId:long}/make-account"), Authorize(Roles = "Solver")]
+    public async Task<IActionResult> SetActiveAccount(long accountId)
+    {
+        AuthorizationCredentials credentials = new(User);
+        Infrastructure.VcsAccount? account = await _context.VcsAccounts.SingleOrDefaultAsync(a => a.Id == accountId);
+        if (account is null)
+            return Problem(detail: $"Account with id {accountId} does not exist.", statusCode: StatusCodes.Status400BadRequest, title: "Cannot set active account.");
+        if (account.UserId != credentials.UserId)
+            return Forbid();
+        Infrastructure.ActiveVcsAccount? activeAccount = await _context.ActiveVcsAccounts.SingleOrDefaultAsync(a => a.UserId == credentials.UserId);
+        if (activeAccount is not null)
+            _context.Remove(activeAccount);
+        Infrastructure.ActiveVcsAccount newActiveAccount = new()
+        {
+            UserId = credentials.UserId,
+            VcsAccountId = accountId
+        };
+        _context.Add(newActiveAccount);
+        return Ok();
+    }
+
     [HttpDelete("{accountId:long}"), Authorize(Roles = "Solver")]
     public async Task<IActionResult> Delete(long accountId)
     {
